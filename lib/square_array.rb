e2b78eea6db83419c1d3515518d48aa9f2346926
Taylor Watson <taylorwatsonb@gmail.com>
2019-08-03 17:33:24 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
-end
+ squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
+  end 
